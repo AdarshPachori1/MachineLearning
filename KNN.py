bc447cd23d77286be7f84998bda137bb3660730e
Adarsh Pachori <adarshpachori21@gmail.com>
2020-04-26 10:11:11 -0700

    Using K-Nearest Neighbor model to train data

---
+++ KNN.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+def KNNClassification():
+    #loading data
+    df = pd.read_csv("data/Classified Data",index_col=0)
+    
+    #data visualization
+    sns.pairplot(data = df, hue = 'TARGET CLASS')
+    
+    #scaling data
+    df_scaled = StandardScaler()
+    df_scaled.fit(df.drop('TARGET CLASS', axis = 1))
+    transformed_features = df_scaled.transform(df.drop('TARGET CLASS', axis =1))
+    dataframe_scaled = pd.DataFrame(transformed_features, columns = df.columns.drop("TARGET CLASS"))
+   
+    #Splitting Data 
+    x_train, x_test, y_train, y_test = train_test_split(dataframe_scaled, df['TARGET CLASS'], test_size = 0.3)
+    knn = KNeighborsClassifier(n_neighbors = 1)
+    knn.fit(x_train, y_train)
+    predictions = knn.predict(x_test)
+    
+    #printing accuracy of our model
+    print("\n\nConfusion Matrix\n")
+    print(confusion_matrix(y_test, predictions))
+    print("\nClassification Report\n")
+    print(classification_report(y_test, predictions))
+    
+    
+if __name__ == "__main__":
+    KNNClassification()
